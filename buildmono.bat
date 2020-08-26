207246c4babb39076305a999fbfcc513d3247607
kilojool <hoglund.joel@gmail.com>
2020-08-26 09:16:44 +0200

    buildmono script changed

--- buildmono.bat
+++ buildmono.bat
@@ -2,4 +2,4 @@ scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j6
 
 bin\godot.windows.tools.64.mono --generate-mono-glue modules/mono/glue
 
-scons p=windows target=release_debug tools=yes module_mono_enabled=yes -j6
+scons p=windows target=release_debug tools=yes module_mono_enabled=yes copy_mono_root=yes -j6 
